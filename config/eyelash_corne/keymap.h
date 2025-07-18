2dbcf6cc1085b9d92fa3172876c8e7680e44cfa4
Stanislav Biche-ool <stas@biche-ool.ru>
2025-07-18 18:02:55 +0300

    Убрал тестовые комбо, обновил матрицу

--- config/eyelash_corne/keymap.h
+++ config/eyelash_corne/keymap.h
@@ -1,11 +1,18 @@
 /*                                      42 KEY MATRIX / LAYOUT MAPPING
+  ╭──────────────────────────────┬───────────────────────────────────────╮
+  │  0   1   2   3   4   5       │        6        7   8   9  10  11  12 │
+  │ 13  14  15  16  17  18       │   19  20  21    22  23  24 25  26  27 │
+  │ 28  29  30  31  32  33   34  │       35        36  37  38 39  40  41 │
+  ╰───────────╮ 42  43  44       │                 45  46  47 ╭──────────╯
+              ╰──────────────────┴────────────────────────────╯
 
-  ╭────────────────────────┬──────────────────────────────────────╮ ╭──────────────────────────┬──────────────────────────────────────╮
-  │  0   1   2   3   4   5 │        6        7   8   9  10  11  12│ │  LT5 LT4 LT3 LT2 LT1 LT0 │     LS0      RT0 RT1 RT2 RT3 RT4 RT5 │
-  │ 13  14  15  16  17  18 │   19  20  21    22  23  24 25  26  27│ │  LM5 LM4 LM3 LM2 LM1 LM0 │ LS1 LS2 LS3  RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 28  29  30  31  32  33 │       34        35  36  37 38  39  40│ │  LB5 LB4 LB3 LB2 LB1 LB0 │     LS4      RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───────────╮ 41  42  43 │                 44  45  46 ╭─────────╯ ╰───────────╮  LH2 LH1 LH0 │              RH0 RH1 RH2 ╭───────────╯
-              ╰────────────┴────────────────────────────╯                       ╰──────────────┴──────────────────────────╯             */
+  ╭──────────────────────────────┬───────────────────────────────────────╮
+  │ LT5 LT4 LT3 LT2 LT1 LT0      │      LS0      RT0 RT1 RT2 RT3 RT4 RT5 │
+  │ LM5 LM4 LM3 LM2 LM1 LM0      │  LS1 LS2 LS3  RM0 RM1 RM2 RM3 RM4 RM5 │
+  │ LB5 LB4 LB3 LB2 LB1 LB0 EN0  │      LS4      RB0 RB1 RB2 RB3 RB4 RB5 │
+  ╰───────────╮ LH2 LH1 LH0      │               RH0 RH1 RH2 ╭───────────╯
+              ╰──────────────────┴───────────────────────────╯
+*/
 
 #pragma once
 
@@ -13,7 +20,9 @@
 #define LS1  19
 #define LS2  20
 #define LS3  21
-#define LS4  34
+#define LS4  35
+
+#define EN0  34 // encoder
 
 #define LT0  5  // left-top row
 #define LT1  4
@@ -50,17 +59,17 @@
 #define LB4 29
 #define LB5 28
 
-#define RB0 35  // right-bottom row
-#define RB1 36
-#define RB2 37
-#define RB3 38
-#define RB4 39
-#define RB5 40
+#define RB0 36  // right-bottom row
+#define RB1 37
+#define RB2 38
+#define RB3 39
+#define RB4 40
+#define RB5 41
 
-#define LH0 43  // left thumb keys
-#define LH1 42
-#define LH2 41
+#define LH0 44  // left thumb keys
+#define LH1 43
+#define LH2 42
 
-#define RH0 44  // right thumb keys
-#define RH1 45
-#define RH2 46
+#define RH0 45  // right thumb keys
+#define RH1 46
+#define RH2 47
